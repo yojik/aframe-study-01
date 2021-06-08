8c21e15c0606643dd5d74025b7e3686fe929e7dc
yojik <yoji.kanno@gmail.com>
2021-06-08 05:43:19 +0000

    socket io の使い方ミスってた

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 
 const express = require('express');
 const app = express();
+var http = require('http').createServer(app);
+const io = require("socket.io")(http)
+// const PORT = process.env.PORT || 7000;
+
 const rootPath = 'docs';
 app.use(express.static(rootPath, { index: false }));
-//index.htmlを勝手に見に行かない指定
 
+var players = []
+
+
+//index.htmlを勝手に見に行かない指定
 const serveIndex = require('serve-index');
 app.use(serveIndex(rootPath, {
   icons: true,
   view: 'details'
 }));
 
-app.listen(8080, ()=> {
+//SocketIOの待ち受け
+io.on('connection',function(socket){
+    console.log('connected');
+});
+
+//appじゃなくてhttp
+http.listen(8080,()=> {
   console.log('Start Express Server');
 });
