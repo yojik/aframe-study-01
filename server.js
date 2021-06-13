a98e190f2a67ec02a5e40f7ac2149a12e68e4da3
yojik <yoji.kanno@gmail.com>
2021-06-13 08:02:52 +0000

    途中]

--- server.js
+++ server.js
@@ -19,26 +19,42 @@ app.use(serveIndex(rootPath, {
 }));
 
 //SocketIOの待ち受け
-io.on('connection',function(socket){
-    socket.on("connected", function () {
-        if(players.length<2){
-            var playerNum = players.length + 1;
-            const p = {no:playerNum, id:socket.id  }
-            players.push(p)
-            io.to(socket.id).emit("set_player", p)
-            return;
-        }
-    });
-    socket.on("disconnect", function () {
-        const result = players.filter(p =>  p.id != socket.id);
-        players = result; 
-    });
+io.on('connection', function (socket) {
+  socket.on("connected", function () {
+    //fitst_player
+    if (players.length == 0) {
+      const p = { no: 1, id: socket.id }
+      players.push(p)
+      io.to(socket.id).emit("set_player", p)
+      return;
+    }
+    //secoud_player or next_player
+    if (players.length == 1) {
+      const other_player = players[0]
+      //すでに入っているプレイヤーの番号が1なら2, 2なら1
+      const num = other_player.no == 1 ? 2 : 1
+      const p = { no: num, id: socket.id }
+      players.push(p)
+      io.to(socket.id).emit("set_player", p)
+      return;
+    }
+  });
+  socket.on("camera_tracking", function (camera_data) {
+    //console.log(camera_data)
+  });
+
+
+
+  socket.on("disconnect", function () {
+    const result = players.filter(p => p.id != socket.id);
+    players = result;
+  });
 
 });
 
 //appじゃなくてhttp
 // WARNING !!! app.listen(8080); will not work here, as it creates a new HTTP server
 // https://socket.io/docs/v3/server-initialization/
-http.listen(8080,()=> {
+http.listen(8080, () => {
   console.log('Start Express Server');
 });
